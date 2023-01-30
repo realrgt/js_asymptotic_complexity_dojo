b22e3e644f3ce27d68b460ce6abeed4743cbc0d4
realrgt <ergitorubildo@gmail.com>
2023-01-30 11:02:00 +0200

    complete the exercise

--- src/1-perm-missing-element/try.js
+++ src/1-perm-missing-element/try.js
@@ -1,1 +1,18 @@
-console.log('perm-missing-element')
+const givenElements = [2, 0, 4, 7, 8, 1, 5, 3, 6, 10];
+
+console.log(solution(givenElements));
+
+function solution(a) {
+  const sortedElements = a.sort((x, y) => x - y);
+
+  for (let i = 0; i < sortedElements.length; i++) {
+    if (i == sortedElements.length) return;
+
+    const currentElement = sortedElements[i];
+    const followingElement = sortedElements[i + 1];
+
+    if (followingElement - currentElement > 1) return sortedElements[i] + 1;
+  }
+
+  return -1;
+}
